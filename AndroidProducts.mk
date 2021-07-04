a51e8206c81c3e95cbf0aa0a72d3134f929cd9ba
xddeath69 <gnogueira316@gmail.com>
2021-07-04 03:02:06 +0000

    Revert "santoni: Switch custom HALs repos"
    This reverts commit 8711b392555cb65fd80c88f6e5d935469665eda0.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
-# LineageOS (crDroid)
+# corvus
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/corvus_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    corvus_santoni-userdebug \
+    corvus_santoni-user \
+    corvus_santoni-eng
 
 # HavocOS
 PRODUCT_MAKEFILES += \
